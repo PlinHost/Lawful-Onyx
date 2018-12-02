Baystation12/Baystation12

Ports Lohikar's unit test subsystem from Aurora.

code/unit_tests/unit_test.dm
@@ -31,7 +31,6 @@ var/all_unit_tests_passed = 1
 var/failed_unit_tests = 0
 var/skipped_unit_tests = 0
 var/total_unit_tests = 0
-var/currently_running_tests = 0
 
 // For console out put in Linux/Bash makes the output green or red.
 // Should probably only be used for unit tests/Travis since some special folks use winders to host servers.
@@ -116,146 +115,46 @@ proc/load_unit_test_changes()
 		config.generate_map = 1	// The default map requires it, the example config doesn't have this enabled.
  */
 
-
-
-
-/proc/initialize_unit_tests()
-	#ifndef UNIT_TEST_COLOURED
-	if(world.system_type != UNIX) // Not a Unix/Linux/etc system, we probably don't want to print color escapes (unless UNIT_TEST_COLOURED was defined to force escapes)
-		ascii_esc = ""
-		ascii_red = ""
-		ascii_green = ""
-		ascii_yellow = ""
-		ascii_reset = ""
-	#endif
-
-	log_unit_test("Initializing Unit Testing")
-
-	//
-	//Start the Round.
-	//
-
-	if(!ticker)
-		crash_with("No Ticker")
-		del(world)
-
-	var/said_msg = 0
-	while(ticker.pregame_timeleft && ticker.pregame_timeleft > 160) 	// Make sure the initial startup is complete.
-		if(ticker.pregame_timeleft < 175 && !said_msg)
-			said_msg = 1
-			log_unit_test("Pregame Count down has started, giving it 20 seconds to finish.")
-		sleep(world.tick_lag)
-
-	log_unit_test("Awaiting the master process...")
-	while(Master.current_runlevel < RUNLEVEL_LOBBY)
-		sleep(world.tick_lag)
-	log_unit_test("Master process setup.")
-
-	world.save_mode("extended")
-
-	sleep(1)
-
-	ticker.current_state = GAME_STATE_SETTING_UP
-	Master.SetRunLevel(RUNLEVEL_SETUP)
-
-	log_unit_test("Round has been started.  Waiting 10 seconds to start tests.")
-	sleep(100)
-	log_unit_test("Initiating tests.")
-
-	//
-	// Run Tests
-	//
-
-	var/list/test_datums = get_test_datums()
-	run_unit_tests(test_datums)
-	log_unit_test("Caught [GLOB.total_runtimes] Runtime\s.")
-	del(world)
-
-/proc/run_unit_tests(var/list/test_datums, var/skip_disabled_tests = TRUE)
-	if(currently_running_tests)
-		log_unit_test("Already running unit tests")
-		return
-	currently_running_tests = 1
-
-	all_unit_tests_passed = 1
-	failed_unit_tests = 0
-	total_unit_tests = 0
-
-	var/list/async_test = list()
-	var/list/started_tests = list()
-
-	log_unit_test("Testing Started.")
-
-	var/end_unit_tests = world.time + MAX_UNIT_TEST_RUN_TIME
-
-	for (var/test in test_datums)
-		var/datum/unit_test/d = new test()
-
-		if(d.disabled && skip_disabled_tests)
-			d.pass("[ascii_red]Check Disabled: [d.why_disabled]")
-			continue
-
-		total_unit_tests++
-		started_tests.Add(d)
-
-		if(world.time > end_unit_tests)
-			d.fail("Unit Tests Ran out of time")   // This should never happen, and if it does either fix your unit tests to be faster or if you can make them async checks.
-			continue
-
-		if(isnull(d.start_test()))		// Start the test.
-			d.fail("Test Runtimed")
-			continue
-		if(d.async)				// If it's async then we'll need to check back on it later.
-			async_test.Add(d)
-
-	//
-	// Check the async tests to see if they are finished.
-	//
-
-	while(async_test.len)
-
-		for(var/datum/unit_test/test  in async_test)
-
-			if(world.time > end_unit_tests)
-				test.fail("Unit Tests Ran out of Time")  // If we're going to run out of time, most likely it's here.  If you can't speed up your unit tests then add time to the timeout at the top.
-				async_test.Remove(test)
-				continue
-
-			var/result = test.check_result()	// Run the async check and store the return
-
-			if(isnull(result))
-				test.fail("Test Runtimed")
-				async_test.Remove(test)
-				continue
-			if(result)				// 0 Means come back, 1 means we got results so move on.
-				async_test.Remove(test)
-		sleep(1)
-
-	//
-	// Make sure all Unit Tests reported a result
-	//
-
-	for(var/datum/unit_test/test in started_tests)
-		if(!test.reported)
-			test.fail("Test failed to report a result.")
-
-	var/skipped_message = ""
-	if(skipped_unit_tests)
-		skipped_message = "| \[[skipped_unit_tests]\\[total_unit_tests]\] Unit Tests Skipped "
-
-	if(all_unit_tests_passed)
-		log_unit_test("[ascii_green]**** All Unit Tests Passed \[[total_unit_tests]\] [skipped_message]****[ascii_reset]")
-	else
-		log_unit_test("[ascii_red]**** \[[failed_unit_tests]\\[total_unit_tests]\] Unit Tests Failed [skipped_message]****[ascii_reset]")
-	currently_running_tests = 0
-
 /proc/get_test_datums()
 	var/list/tests = list()
 	for(var/test in typesof(/datum/unit_test))
 		var/datum/unit_test/d = test
-		if(!findtext(initial(d.name), "template"))
-			tests += d
+		if(findtext(initial(d.name), "template"))
+			continue
+		tests += d
 	return tests
+
+/proc/do_unit_test(datum/unit_test/test, end_time, skip_disabled_tests = TRUE)
+	if(test.disabled && skip_disabled_tests)
+		test.pass("[ascii_red]Check Disabled: [test.why_disabled]")
+		return
+	if(world.time > end_time)
+		test.fail("Unit Tests Ran out of time")   // This should never happen, and if it does either fix your unit tests to be faster or if you can make them async checks.
+		return
+	if (test.start_test() == null)	// Runtimed.
+		test.fail("Test Runtimed")
+	return 1
+
+//For async tests. Returns 1 if done.
+/proc/check_unit_test(datum/unit_test/test, end_time)
+	if(world.time > end_time)
+		test.fail("Unit Tests Ran out of Time")// If we're going to run out of time, most likely it's here.  If you can't speed up your unit tests then add time to the timeout at the top.
+		return 1
+	var/result = test.check_result()
+	if(isnull(result))
+		test.fail("Test Runtimed")
+		return 1
+	else if(result)
+		return 1
+
+/proc/unit_test_final_message()
+	var/skipped_message = ""
+	if(skipped_unit_tests)
+		skipped_message = "| \[[skipped_unit_tests]\\[total_unit_tests]\] Unit Tests Skipped "
+	if(all_unit_tests_passed)
+		log_unit_test("[ascii_green]**** All Unit Tests Passed \[[total_unit_tests]\] [skipped_message]****[ascii_reset]")
+	else
+		log_unit_test("[ascii_red]**** \[[failed_unit_tests]\\[total_unit_tests]\] Unit Tests Failed [skipped_message]****[ascii_reset]")
 
 /datum/admins/proc/run_unit_test(var/datum/unit_test/unit_test_type in get_test_datums())
 	set name = "Run Unit Test"
@@ -269,6 +168,113 @@ proc/load_unit_test_changes()
 		return
 
 	log_and_message_admins("has started the unit test '[initial(unit_test_type.name)]'")
-	run_unit_tests(list(unit_test_type), FALSE)
+	var/datum/unit_test/test = new unit_test_type
+	var/end_unit_tests = world.time + MAX_UNIT_TEST_RUN_TIME
+	do_unit_test(test, end_unit_tests, FALSE)
+	if(test.async)
+		while(!check_unit_test(test, end_unit_tests))
+			sleep(20)
+	unit_test_final_message()
 
+
+#ifdef UNIT_TEST
+
+SUBSYSTEM_DEF(unit_tests)
+	name = "Unit Tests"
+	wait = 2 SECONDS
+	init_order = INIT_ORDER_UNIT_TESTS
+	runlevels = (RUNLEVELS_DEFAULT | RUNLEVEL_LOBBY)
+	var/list/queue = list()
+	var/list/async_tests = list()
+	var/list/current_async
+	var/stage = 0
+	var/end_unit_tests
+
+/datum/controller/subsystem/unit_tests/Initialize(timeofday)
+	#ifndef UNIT_TEST_COLOURED
+	if(world.system_type != UNIX) // Not a Unix/Linux/etc system, we probably don't want to print color escapes (unless UNIT_TEST_COLOURED was defined to force escapes)
+		ascii_esc = ""
+		ascii_red = ""
+		ascii_green = ""
+		ascii_yellow = ""
+		ascii_reset = ""
+	#endif
+	log_unit_test("Initializing Unit Testing")
+	//
+	//Start the Round.
+	//
+	world.save_mode("extended")
+	for(var/test_datum_type in get_test_datums())
+		queue += new test_datum_type
+	log_unit_test("[queue.len] unit tests loaded.")
+	. = ..()
+
+/datum/controller/subsystem/unit_tests/proc/start_game()
+	if(Master.current_runlevel < RUNLEVEL_LOBBY)
+		return //Have to wait for the old Master.
+	log_unit_test("Master process setup.")
+
+	if (ticker.current_state == GAME_STATE_PREGAME)
+		ticker.current_state = GAME_STATE_SETTING_UP
+		Master.SetRunLevel(RUNLEVEL_SETUP)
+		stage++
+		log_unit_test("Round has been started.  Waiting 10 seconds to start tests.")
+		postpone(5)
+	else
+		log_unit_test("Unable to start testing; ticker.current_state=[ticker.current_state]!")
+		del world
+
+/datum/controller/subsystem/unit_tests/proc/handle_tests()
+	var/list/curr = queue
+	while (curr.len)
+		var/datum/unit_test/test = curr[curr.len]
+		curr.len--
+		if(do_unit_test(test, end_unit_tests) && test.async)
+			async_tests += test
+		total_unit_tests++
+		if (MC_TICK_CHECK)
+			return
+	if (!curr.len)
+		stage++
+
+/datum/controller/subsystem/unit_tests/proc/handle_async(resumed = 0)
+	if (!resumed)
+		current_async = async_tests.Copy()
+
+	var/list/async = current_async
+	while (async.len)
+		var/datum/unit_test/test = current_async[current_async.len]
+		current_async.len--
+		if(check_unit_test(test, end_unit_tests))
+			async_tests -= test
+		if (MC_TICK_CHECK)
+			return
+	if (!async.len)
+		stage++
+
+/datum/controller/subsystem/unit_tests/fire(resumed = 0)
+	switch (stage)
+		if (0)
+			stage ++
+			log_unit_test("Awaiting the master process...")
+
+		if (1)
+			start_game()
+
+		if (2)	// wait a moment
+			stage++
+			log_unit_test("Testing Started.")
+			end_unit_tests = world.time + MAX_UNIT_TEST_RUN_TIME
+
+		if (3)	// do normal tests
+			handle_tests()
+
+		if (4)
+			handle_async(resumed)
+
+		if (5)	// Finalization.
+			unit_test_final_message()
+			log_unit_test("Caught [GLOB.total_runtimes] Runtime\s.")
+			del world
+#endif
 #undef MAX_UNIT_TEST_RUN_TIME

PlinHost/Lawful-Onyx

code/unit_tests/unit_test.dm
@@ -31,7 +31,6 @@ var/all_unit_tests_passed = 1
 var/failed_unit_tests = 0
 var/skipped_unit_tests = 0
 var/total_unit_tests = 0
-var/currently_running_tests = 0
 
 // For console out put in Linux/Bash makes the output green or red.
 // Should probably only be used for unit tests/Travis since some special folks use winders to host servers.
@@ -116,146 +115,46 @@ proc/load_unit_test_changes()
 		config.generate_map = 1	// The default map requires it, the example config doesn't have this enabled.
  */
 
-
-
-
-/proc/initialize_unit_tests()
-	#ifndef UNIT_TEST_COLOURED
-	if(world.system_type != UNIX) // Not a Unix/Linux/etc system, we probably don't want to print color escapes (unless UNIT_TEST_COLOURED was defined to force escapes)
-		ascii_esc = ""
-		ascii_red = ""
-		ascii_green = ""
-		ascii_yellow = ""
-		ascii_reset = ""
-	#endif
-
-	log_unit_test("Initializing Unit Testing")
-
-	//
-	//Start the Round.
-	//
-
-	if(!ticker)
-		crash_with("No Ticker")
-		del(world)
-
-	var/said_msg = 0
-	while(ticker.pregame_timeleft && ticker.pregame_timeleft > 160) 	// Make sure the initial startup is complete.
-		if(ticker.pregame_timeleft < 175 && !said_msg)
-			said_msg = 1
-			log_unit_test("Pregame Count down has started, giving it 20 seconds to finish.")
-		sleep(world.tick_lag)
-
-	log_unit_test("Awaiting the master process...")
-	while(Master.current_runlevel < RUNLEVEL_LOBBY)
-		sleep(world.tick_lag)
-	log_unit_test("Master process setup.")
-
-	world.save_mode("extended")
-
-	sleep(1)
-
-	ticker.current_state = GAME_STATE_SETTING_UP
-	Master.SetRunLevel(RUNLEVEL_SETUP)
-
-	log_unit_test("Round has been started.  Waiting 10 seconds to start tests.")
-	sleep(100)
-	log_unit_test("Initiating tests.")
-
-	//
-	// Run Tests
-	//
-
-	var/list/test_datums = get_test_datums()
-	run_unit_tests(test_datums)
-	log_unit_test("Caught [GLOB.total_runtimes] Runtime\s.")
-	del(world)
-
-/proc/run_unit_tests(var/list/test_datums, var/skip_disabled_tests = TRUE)
-	if(currently_running_tests)
-		log_unit_test("Already running unit tests")
-		return
-	currently_running_tests = 1
-
-	all_unit_tests_passed = 1
-	failed_unit_tests = 0
-	total_unit_tests = 0
-
-	var/list/async_test = list()
-	var/list/started_tests = list()
-
-	log_unit_test("Testing Started.")
-
-	var/end_unit_tests = world.time + MAX_UNIT_TEST_RUN_TIME
-
-	for (var/test in test_datums)
-		var/datum/unit_test/d = new test()
-
-		if(d.disabled && skip_disabled_tests)
-			d.pass("[ascii_red]Check Disabled: [d.why_disabled]")
-			continue
-
-		total_unit_tests++
-		started_tests.Add(d)
-
-		if(world.time > end_unit_tests)
-			d.fail("Unit Tests Ran out of time")   // This should never happen, and if it does either fix your unit tests to be faster or if you can make them async checks.
-			continue
-
-		if(isnull(d.start_test()))		// Start the test.
-			d.fail("Test Runtimed")
-			continue
-		if(d.async)				// If it's async then we'll need to check back on it later.
-			async_test.Add(d)
-
-	//
-	// Check the async tests to see if they are finished.
-	//
-
-	while(async_test.len)
-
-		for(var/datum/unit_test/test  in async_test)
-
-			if(world.time > end_unit_tests)
-				test.fail("Unit Tests Ran out of Time")  // If we're going to run out of time, most likely it's here.  If you can't speed up your unit tests then add time to the timeout at the top.
-				async_test.Remove(test)
-				continue
-
-			var/result = test.check_result()	// Run the async check and store the return
-
-			if(isnull(result))
-				test.fail("Test Runtimed")
-				async_test.Remove(test)
-				continue
-			if(result)				// 0 Means come back, 1 means we got results so move on.
-				async_test.Remove(test)
-		sleep(1)
-
-	//
-	// Make sure all Unit Tests reported a result
-	//
-
-	for(var/datum/unit_test/test in started_tests)
-		if(!test.reported)
-			test.fail("Test failed to report a result.")
-
-	var/skipped_message = ""
-	if(skipped_unit_tests)
-		skipped_message = "| \[[skipped_unit_tests]\\[total_unit_tests]\] Unit Tests Skipped "
-
-	if(all_unit_tests_passed)
-		log_unit_test("[ascii_green]**** All Unit Tests Passed \[[total_unit_tests]\] [skipped_message]****[ascii_reset]")
-	else
-		log_unit_test("[ascii_red]**** \[[failed_unit_tests]\\[total_unit_tests]\] Unit Tests Failed [skipped_message]****[ascii_reset]")
-	currently_running_tests = 0
-
 /proc/get_test_datums()
 	var/list/tests = list()
 	for(var/test in typesof(/datum/unit_test))
 		var/datum/unit_test/d = test
-		if(!findtext(initial(d.name), "template"))
-			tests += d
+		if(findtext(initial(d.name), "template"))
+			continue
+		tests += d
 	return tests
+
+/proc/do_unit_test(datum/unit_test/test, end_time, skip_disabled_tests = TRUE)
+	if(test.disabled && skip_disabled_tests)
+		test.pass("[ascii_red]Check Disabled: [test.why_disabled]")
+		return
+	if(world.time > end_time)
+		test.fail("Unit Tests Ran out of time")   // This should never happen, and if it does either fix your unit tests to be faster or if you can make them async checks.
+		return
+	if (test.start_test() == null)	// Runtimed.
+		test.fail("Test Runtimed")
+	return 1
+
+//For async tests. Returns 1 if done.
+/proc/check_unit_test(datum/unit_test/test, end_time)
+	if(world.time > end_time)
+		test.fail("Unit Tests Ran out of Time")// If we're going to run out of time, most likely it's here.  If you can't speed up your unit tests then add time to the timeout at the top.
+		return 1
+	var/result = test.check_result()
+	if(isnull(result))
+		test.fail("Test Runtimed")
+		return 1
+	else if(result)
+		return 1
+
+/proc/unit_test_final_message()
+	var/skipped_message = ""
+	if(skipped_unit_tests)
+		skipped_message = "| \[[skipped_unit_tests]\\[total_unit_tests]\] Unit Tests Skipped "
+	if(all_unit_tests_passed)
+		log_unit_test("[ascii_green]**** All Unit Tests Passed \[[total_unit_tests]\] [skipped_message]****[ascii_reset]")
+	else
+		log_unit_test("[ascii_red]**** \[[failed_unit_tests]\\[total_unit_tests]\] Unit Tests Failed [skipped_message]****[ascii_reset]")
 
 /datum/admins/proc/run_unit_test(var/datum/unit_test/unit_test_type in get_test_datums())
 	set name = "Run Unit Test"
@@ -269,6 +168,113 @@ proc/load_unit_test_changes()
 		return
 
 	log_and_message_admins("has started the unit test '[initial(unit_test_type.name)]'")
-	run_unit_tests(list(unit_test_type), FALSE)
+	var/datum/unit_test/test = new unit_test_type
+	var/end_unit_tests = world.time + MAX_UNIT_TEST_RUN_TIME
+	do_unit_test(test, end_unit_tests, FALSE)
+	if(test.async)
+		while(!check_unit_test(test, end_unit_tests))
+			sleep(20)
+	unit_test_final_message()
 
+
+#ifdef UNIT_TEST
+
+SUBSYSTEM_DEF(unit_tests)
+	name = "Unit Tests"
+	wait = 2 SECONDS
+	init_order = SS_INIT_UNIT_TESTS
+	runlevels = (RUNLEVELS_DEFAULT | RUNLEVEL_LOBBY)
+	var/list/queue = list()
+	var/list/async_tests = list()
+	var/list/current_async
+	var/stage = 0
+	var/end_unit_tests
+
+/datum/controller/subsystem/unit_tests/Initialize(timeofday)
+	#ifndef UNIT_TEST_COLOURED
+	if(world.system_type != UNIX) // Not a Unix/Linux/etc system, we probably don't want to print color escapes (unless UNIT_TEST_COLOURED was defined to force escapes)
+		ascii_esc = ""
+		ascii_red = ""
+		ascii_green = ""
+		ascii_yellow = ""
+		ascii_reset = ""
+	#endif
+	log_unit_test("Initializing Unit Testing")
+	//
+	//Start the Round.
+	//
+	world.save_mode("extended")
+	for(var/test_datum_type in get_test_datums())
+		queue += new test_datum_type
+	log_unit_test("[queue.len] unit tests loaded.")
+	. = ..()
+
+/datum/controller/subsystem/unit_tests/proc/start_game()
+	if(Master.current_runlevel < RUNLEVEL_LOBBY)
+		return //Have to wait for the old Master.
+	log_unit_test("Master process setup.")
+
+	if (ticker.current_state == GAME_STATE_PREGAME)
+		ticker.current_state = GAME_STATE_SETTING_UP
+		Master.SetRunLevel(RUNLEVEL_SETUP)
+		stage++
+		log_unit_test("Round has been started.  Waiting 10 seconds to start tests.")
+		postpone(5)
+	else
+		log_unit_test("Unable to start testing; ticker.current_state=[ticker.current_state]!")
+		del world
+
+/datum/controller/subsystem/unit_tests/proc/handle_tests()
+	var/list/curr = queue
+	while (curr.len)
+		var/datum/unit_test/test = curr[curr.len]
+		curr.len--
+		if(do_unit_test(test, end_unit_tests) && test.async)
+			async_tests += test
+		total_unit_tests++
+		if (MC_TICK_CHECK)
+			return
+	if (!curr.len)
+		stage++
+
+/datum/controller/subsystem/unit_tests/proc/handle_async(resumed = 0)
+	if (!resumed)
+		current_async = async_tests.Copy()
+
+	var/list/async = current_async
+	while (async.len)
+		var/datum/unit_test/test = current_async[current_async.len]
+		current_async.len--
+		if(check_unit_test(test, end_unit_tests))
+			async_tests -= test
+		if (MC_TICK_CHECK)
+			return
+	if (!async.len)
+		stage++
+
+/datum/controller/subsystem/unit_tests/fire(resumed = 0)
+	switch (stage)
+		if (0)
+			stage ++
+			log_unit_test("Awaiting the master process...")
+
+		if (1)
+			start_game()
+
+		if (2)	// wait a moment
+			stage++
+			log_unit_test("Testing Started.")
+			end_unit_tests = world.time + MAX_UNIT_TEST_RUN_TIME
+
+		if (3)	// do normal tests
+			handle_tests()
+
+		if (4)
+			handle_async(resumed)
+
+		if (5)	// Finalization.
+			unit_test_final_message()
+			log_unit_test("Caught [GLOB.total_runtimes] Runtime\s.")
+			del world
+#endif
 #undef MAX_UNIT_TEST_RUN_TIME